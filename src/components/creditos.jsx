357b202330229008ad42a35e049cc3d26130d848
baktcl <kampf@live.cl>
2026-02-21 18:25:36 -0300

    fix copyright responsivo

--- src/components/creditos.jsx
+++ src/components/creditos.jsx
@@ -5,7 +5,8 @@ function Creditos() {
             <a href="https://bakt.cl/">
                 <img src={baktlogo} width="25" height="25" alt="bakt.cl" class="logotipo" />
             </a>
-            <p>Sitio web desarrollado en React. Diseño y codificación por </p>
+            <p>Sitio web desarrollado en React.</p>
+            <p>Diseño y codificación por:</p>
             <a href="https://bakt.cl/">Bryan Köhnenkampf</a>
         </div>
     )
