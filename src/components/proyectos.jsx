db5a6bf19fdc91273ea1a5393ceb92a102705109
baktcl <kampf@live.cl>
2026-07-06 02:42:53 -0400

    fix final

--- src/components/proyectos.jsx
+++ src/components/proyectos.jsx
@@ -5,7 +5,7 @@ function Proyectos() {
         <section class="proyectos full-screen d-lg-flex justify-content-center align-items-center" id="Proyectos">
             <div class="container">
                 <div class="row"> 
-                    <p class="leyenda">
+                    <p class="p-5 m-0 leyenda">
                         Mi trabajo es cubrir el espectro UI completo — diseño visual, implementación front-end y motion — e implementar el código en producción. En UX mi enfoque es práctico: benchmarking competitivo, arquitectura de flujos y validación con prototipos orientados a conversión.
                     </p>
                 </div>
